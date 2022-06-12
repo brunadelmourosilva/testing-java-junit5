ad9b0e8613c3a4c2c8c490a6217fce1c1ccd8bf4
Bruna <brunadelmouro@gmail.com>
2022-06-12 14:01:28 -0300

    JUnit Test Display Names

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,12 +15,14 @@ class IndexControllerTest {
         indexController = new IndexController();
     }
 
+    @DisplayName("The Proper View name is returned for index page")
     @Test
     void index() {
         assertEquals("index", indexController.index());
         assertEquals("index", indexController.index(), "Make me only if you have to");
     }
 
+    @DisplayName("Test exception")
     @Test
     void oupsHandler() {
         //exception handler
